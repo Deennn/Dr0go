7dc45d9972c48170f827d67a05920ed0b58b53b2
User <user@Users-MacBook-Air.local>
2022-02-02 13:14:34 +0100

    add some comment to verify something

--- src/dr0go/com/iCeo.java
+++ src/dr0go/com/iCeo.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface iCeo {
     public List<Employee> hire(List<Applicants> applicants);
     public  String fire(Employee employee);
+//    let me test something;
 }
